ca9f5d23096397f52f31e9b186d0f833ccc77c67
Arthur Rambo <arthurbrachtrambo@gmail.com>
2021-06-25 18:39:28 -0300

    Aplicativo para armazenar e calcular notas da escola.

---
+++ AplicativoBoletim/Disciplina.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace AplicativoBoletim
+{
+    partial class Disciplina
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtProva1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtProva2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.txtProva1,
+            this.txtProva2});
+            this.dataGridView1.Location = new System.Drawing.Point(180, 141);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtProva1
+            // 
+            this.txtProva1.HeaderText = "Prova 1";
+            this.txtProva1.Name = "txtProva1";
+            this.txtProva1.ReadOnly = true;
+            // 
+            // txtProva2
+            // 
+            this.txtProva2.HeaderText = "Prova 2";
+            this.txtProva2.Name = "txtProva2";
+            this.txtProva2.ReadOnly = true;
+            // 
+            // Disciplina
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Disciplina";
+            this.Text = "Disciplina";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txtProva1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn txtProva2;
+    }
+}
